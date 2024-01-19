2889125bc890bc59769d527649c0dad1bf692474
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-18 22:51:00 -0800

    Had to use AI to help with error. Didnt realize I needed the script after my inputs and vaulues and now code works just fine. Want to tinker more to really understand what is going on

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 // Square the Number
 // Spit out number in DOM 
 
-const input = document.querySelector("myInput");
+const input = document.querySelector(".myInput");
 const para = document.querySelector('p');
+
+function squared(num) {
+    return num * num;
+}
 
 input.addEventListener("change", () => {
     const num = parseFloat(input.value);
     if (isNaN(num)) {
         para.textContent = "Please enter a number";
+    } else {
+        para.textContent = `The sqare of ${num} is ${squared(num)}.`
     }
 
 })
 
-console.log(num);
